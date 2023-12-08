f4037585d3425175a4b601d24b1cbc1fce690774
AnthonyPacamarra <anthonygwapopacamarra@gmail.com>
2023-12-09 03:03:00 +0800

    updated (receipt and other minor changes)

--- src/app/user-main-feed/car-rental/car-rental.component.ts
+++ src/app/user-main-feed/car-rental/car-rental.component.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
 })
 export class CarRentalComponent implements OnInit {
   carDetails: any;
+
   pickupDate: string = '';
   returnDate: string = '';
   pickupTime: string = '';
@@ -17,6 +18,9 @@ export class CarRentalComponent implements OnInit {
   rentalCost: number = 0;
   extraPayment: number = 0;
   totalExpense: number = 0;
+  daysDifference: number = 0;
+  currentCarId: string = ''; //ilisanan pa ni
+  currentUserId: string = ''; //ilisanan pa ni
 
   selectedExtras: any = {
     gpsNavigation: false,
@@ -24,6 +28,7 @@ export class CarRentalComponent implements OnInit {
     childSeat: false,
     roofBicycleRack: false,
   };
+
   extraPrices: any = {
     gpsNavigation: 100,
     additionalDriver: 250,
@@ -32,25 +37,29 @@ export class CarRentalComponent implements OnInit {
   };
 
   transactionDetails: TransactionDetails = {
-    transactionId: '',
-    user: '',
+    transactionUserId: '',
+    transactionCarId: '',
+    startDate: '',
+    startTime: '',
+    endDate: '',
+    endTime: '',
     rent: 0,
     extras: [],
+    duration: 0,
     total: 0,
   };
 
   constructor(private firestore: AngularFirestore, private router: Router) {}
 
   ngOnInit() {
+    this.currentCarId = 'nkcd5S3lJVMfJPx5VQLA'; //ilisanan pa ni
+    this.currentUserId = '9zOAMpevRPcqWJLi84C6DH54hap2'; //ilisanan pa ni
     this.getCarDetails();
   }
-
   getCarDetails() {
-    const carId = 'NQqYOoSMrxt5aUiuOoIC';//ilisanan na ni diria
-
     this.firestore
       .collection('car-inventory')
-      .doc(carId)
+      .doc(this.currentCarId)
       .valueChanges()
       .subscribe((data: any) => {
         this.carDetails = data;
@@ -58,50 +67,40 @@ export class CarRentalComponent implements OnInit {
       }
     );
   }
-
+  calculateDays() {
+    const formattedPickupTime = this.pickupTime.toString();
+    const formattedReturnTime = this.returnTime.toString();
+    const pickupDateTime = new Date(this.pickupDate + 'T' + formattedPickupTime + ':00');
+    const returnDateTime = new Date(this.returnDate + 'T' + formattedReturnTime + ':00');
+    const timeDifference = returnDateTime.getTime() - pickupDateTime.getTime();
+    const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    return daysDifference;
+  }
   calculateRentalCost() {
-    const formattedPickupTime = this.padZero(this.pickupTime.toString());
-    const formattedReturnTime = this.padZero(this.returnTime.toString());
-    const pickupDateTime = new Date(this.pickupDate + 'T' + formattedPickupTime + ':00:00');
-    const returnDateTime = new Date(this.returnDate + 'T' + formattedReturnTime + ':00:00');
-    const timeDifference = returnDateTime.getTime() - pickupDateTime.getTime();
-    const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
-
-    this.rentalCost = daysDifference * this.carDetails?.rentPrice || 0;
+    this.rentalCost = this.daysDifference * this.carDetails?.rentPrice || 0;
   }
-  padZero(value: string): string {
-    return value.length === 1 ? '0' + value : value;
-  }
-
   calculateTotalExtraPayment() {
-    const formattedPickupTime = this.padZero(this.pickupTime.toString());
-    const formattedReturnTime = this.padZero(this.returnTime.toString());
-    const pickupDateTime = new Date(this.pickupDate + 'T' + formattedPickupTime + ':00:00');
-    const returnDateTime = new Date(this.returnDate + 'T' + formattedReturnTime + ':00:00');
-    const timeDifference = returnDateTime.getTime() - pickupDateTime.getTime();
-    const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
-
     this.extraPayment = 0;
 
     if (this.selectedExtras.gpsNavigation) {
-      this.extraPayment += this.extraPrices.gpsNavigation * daysDifference;
+      this.extraPayment += this.extraPrices.gpsNavigation * this.daysDifference;
     }
     if (this.selectedExtras.additionalDriver) {
-      this.extraPayment += this.extraPrices.additionalDriver * daysDifference;
+      this.extraPayment += this.extraPrices.additionalDriver * this.daysDifference;
     }
     if (this.selectedExtras.childSeat) {
-      this.extraPayment += this.extraPrices.childSeat * daysDifference;
+      this.extraPayment += this.extraPrices.childSeat * this.daysDifference;
     }
     if (this.selectedExtras.roofBicycleRack) {
-      this.extraPayment += this.extraPrices.roofBicycleRack * daysDifference;
+      this.extraPayment += this.extraPrices.roofBicycleRack * this.daysDifference;
     }
   }
-
   totalRentExpense() {
     this.totalExpense = this.rentalCost + this.extraPayment;
   }
 
   onInputChange() {
+    this.daysDifference = this.calculateDays();
     this.calculateRentalCost();
     this.calculateTotalExtraPayment();
     this.totalRentExpense()
@@ -109,51 +108,86 @@ export class CarRentalComponent implements OnInit {
 
   getSelectedExtrasDetails() {
     const details: ExtraDetail[] = [];
-    const formattedPickupTime = this.padZero(this.pickupTime.toString());
-    const formattedReturnTime = this.padZero(this.returnTime.toString());
-    const pickupDateTime = new Date(this.pickupDate + 'T' + formattedPickupTime + ':00:00');
-    const returnDateTime = new Date(this.returnDate + 'T' + formattedReturnTime + ':00:00');
-    const timeDifference = returnDateTime.getTime() - pickupDateTime.getTime();
-    const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
-
+    
     if (this.selectedExtras.gpsNavigation) {
-      details.push({
+      details.push({    
         name: 'GPS Navigation',
-        cost: this.extraPrices.gpsNavigation * daysDifference,
+        basePrice: this.extraPrices.gpsNavigation,
+        cost: this.extraPrices.gpsNavigation * this.daysDifference,
       });
     }
     if (this.selectedExtras.additionalDriver) {
       details.push({
         name: 'Additional Driver',
-        cost: this.extraPrices.additionalDriver * daysDifference,
+        basePrice: this.extraPrices.additionalDriver,
+        cost: this.extraPrices.additionalDriver * this.daysDifference,
       });
     }
     if (this.selectedExtras.childSeat) {
       details.push({
         name: 'Child seat',
-        cost: this.extraPrices.childSeat * daysDifference,
+        basePrice: this.extraPrices.childSeat,
+        cost: this.extraPrices.childSeat * this.daysDifference,
       });
     }
     if (this.selectedExtras.roofBicycleRack) {
       details.push({
         name: 'Roof bicycle rack',
-        cost: this.extraPrices.roofBicycleRack * daysDifference,
+        basePrice: this.extraPrices.roofBicycleRack,
+        cost: this.extraPrices.roofBicycleRack * this.daysDifference,
       });
     }
     return details;
   }
+  getTime(time: string){
+    switch(time) {
+      case "09:00":
+        time = "09:00 AM"
+        break;
+      case "10:00":
+        time = "10:00 AM"
+        break;
+      case "11:00":
+        time = "11:00 AM"
+        break;
+      case "12:00":
+        time = "12:00 PM"
+        break;
+      case "13:00":
+        time = "01:00 PM"
+        break;
+      case "14:00":
+        time = "02:00 PM"
+        break;
+      case "15:00":
+        time = "03:00 PM"
+        break;
+      case "16:00":
+        time = "04:00 PM"
+        break;
+      case "17:00":
+        time = "05:00 PM"
+        break;
+    }
+    return time
+  }
 
   modelUpdate() {
+    this.transactionDetails.transactionCarId = this.currentCarId;
+    this.transactionDetails.transactionUserId = this.currentUserId;
+    this.transactionDetails.startDate = this.pickupDate;
+    this.transactionDetails.startTime = this.getTime(this.pickupTime);
+    this.transactionDetails.endDate = this.returnDate;
+    this.transactionDetails.endTime = this.getTime(this.returnTime);
     this.transactionDetails.rent = this.rentalCost || 0;
     this.transactionDetails.extras = this.getSelectedExtrasDetails();
     this.transactionDetails.total = this.totalExpense;
+    this.transactionDetails.duration = this.daysDifference;
+    
 
-    this.firestore.collection('transactions').add(this.transactionDetails)
-      .catch((error) => {
-        console.error('Error adding transaction:', error);
-      });
-
-    console.log("Transaction: ",this.transactionDetails);
+    this.firestore.collection('transactions').doc().set(
+      this.transactionDetails
+    );
   }
 
   goToLanding() {
